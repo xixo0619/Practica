8e17e6b7e97959c07ac177ce3c83ccd09247237a
Josue Bonilla <xixojabm@gmail.com>
2019-02-13 14:08:03 -0600

    Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var express = require("express"); //Importar el framework express
+var app = express(); //Inicializar la aplicacion con express
+app.use(express.static("public")); //Publicar un directorio de archivos estaticos
+app.listen(8001,function(){
+    console.log("Servidor iniciado");
+});//Levantar el servidor y escuchar en el puerto indicado

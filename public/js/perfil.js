f2478e2ef078adc0b765bccf2952a5a6492d9dc9
Josue Bonilla <xixojabm@gmail.com>
2019-02-15 13:29:34 -0600

    commit

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -5,3 +5,21 @@ $(function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
 })
+
+
+function modalUsuario() {
+  $("#modalUsuario").modal("show");
+  
+}
+
+function modalCuenta() {
+  $("#modalCuenta").modal("show");
+}
+
+function modalImagen() {
+  $("#modalImagen").modal("show");
+}
+
+function modalSecundario(){
+  $("#modalSecundario").modal("show");
+}
